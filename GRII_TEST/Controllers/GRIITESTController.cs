6e9fc069e499917408626885d56515bd0f68c4ed
william augusta <99062128+williamaugusta@users.noreply.github.com>
2023-10-30 22:00:00 +0700

    w

---
+++ GRII_TEST/Controllers/GRIITESTController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GRII_TEST.Models;
+
+namespace GRII_TEST.Controllers
+{
+    public class GRIITESTController : Controller
+    {
+        // GET: GRIITEST
+        public ActionResult Index()
+        {
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/images_content/"));
+            List<Slider> files = new List<Slider>();
+            foreach (string filePath in filePaths)
+            {
+                string fileName = Path.GetFileName(filePath);
+                files.Add(new Slider
+                {
+                    title = fileName.Split('.')[0].ToString(),
+                    src = "/images_content/" + fileName
+                });
+            }
+
+            return View(files);
+        }
+
+        public ActionResult Visi_Misi()
+        {
+            return View();
+        }
+
+        public ActionResult Pengakuan_Iman()
+        {
+            return View();
+        }
+        public ActionResult Beban_Doa()
+        {
+            return View();
+        }
+        public ActionResult Profil_Hamba_Tuhan()
+        {
+            return View();
+        }
+        public ActionResult Gerakan_Grii()
+        {
+            return View();
+        }
+        public ActionResult Kegiatan()
+        {
+            return View();
+        }
+    }
+}
---
+++ GRII_TEST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GRII_TEST.MvcApplication" Language="C#" %>
