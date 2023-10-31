ae98c5c2d5a3e9409e2a189141984a5a26fdd4d3
william augusta <99062128+williamaugusta@users.noreply.github.com>
2023-10-31 09:20:01 +0700

    w

--- GRII_TEST/Scripts/main.js
+++ GRII_TEST/Scripts/main.js
@@ -184,8 +184,8 @@
 			items: 3,
 			loop: true,
 			margin: 10,
-			nav: true,
-			dots: true,
+			nav: false,
+			dots: false,
 			autoWidth: true,
 			navText: [
 		      "<i class='ti-arrow-left owl-direction'></i>",
@@ -213,8 +213,8 @@
 			nav: true,
 			dots: true,
 			autoplay: true,
-			autoplayTimeout: 5000,
-			autoplayHoverPause: false,
+			autoplayTimeout: 3000,
+			autoplayHoverPause: true,
 			navText: [
 		      "<i class='ti-arrow-left owl-direction'></i>",
 		      "<i class='ti-arrow-right owl-direction'></i>"
